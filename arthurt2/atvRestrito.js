19adb29f9e05614e3dc690463c5838411179f1cc
ArthurzinhoDev <152618050+ArthurzinhoDev@users.noreply.github.com>
2025-03-12 17:02:50 -0300

    Delete arthurt2 directory

--- arthurt2/atvRestrito.js
+++
@@ -1,8 +0,0 @@
-let user = prompt("Qual seu acesso?")
-let senha= prompt("Qual sua senha?")
-
-acesso= "acesso liberado"
-acesso2="acesso negado"
-
-console.log((user=="admin" || user=="moderador" && senha==12345) && acesso || acesso2)
-
