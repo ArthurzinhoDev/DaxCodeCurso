283d8582187e9f6d5c5c6c18143491de550f8791
ArthurzinhoDev <152618050+ArthurzinhoDev@users.noreply.github.com>
2025-03-12 17:03:28 -0300

    Add files via upload

---
+++ atvPortasLog/maior.js
@@ -0,0 +1,7 @@
+a = 10000
+b = 60
+maiora= "O numero A é maior que o numero B"
+igual= "Os numeros sao iguais."
+maiorb= "O numero B é maior que o numero A"
+
+console.log( (a>b && maiora) || (b>a && maiorb) || (a==b && igual) )
